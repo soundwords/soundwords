7be3adbae9cd8440f3e1d32f19fd8ef853825b4a
David A. Sjøen <david@pilgrim.no>
2023-09-10 00:58:32 +0200

    Disabled consent

--- src/SoundWords/Program.cs
+++ src/SoundWords/Program.cs
@@ -31,12 +31,12 @@ try
     builder.Services.AddMvc(options => options.EnableEndpointRouting = false);
 #endif
 
-    builder.Services.Configure<CookiePolicyOptions>(options =>
-                                                    {
-                                                        // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                                                        options.CheckConsentNeeded = context => true;
-                                                        options.MinimumSameSitePolicy = SameSiteMode.None;
-                                                    });
+    // builder.Services.Configure<CookiePolicyOptions>(options =>
+    //                                                 {
+    //                                                     // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+    //                                                     options.CheckConsentNeeded = context => true;
+    //                                                     options.MinimumSameSitePolicy = SameSiteMode.None;
+    //                                                 });
 
     builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
            .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme);
